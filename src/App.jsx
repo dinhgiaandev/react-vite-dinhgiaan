f5befc7e0061d49a62f5be40c716b542351a8fbf
dinhgiaandev <dinhgiaanforwork@gmail.com>
2024-08-09 17:10:23 +0700

    video 28

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,20 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const MyComponent = () => {
+  return (
+    <>
+      <p1>hello</p1>
+    </>
+  );
+}
+
 const App = () => {
   const [count, setCount] = useState(0)
 
-  // function myFunction() {
-  //   console.log('>>Run my function');
-  // }
-
-  const myFunction = () => {
-    console.log('>>Run my function');
-  }
-
-  myFunction();
-
   return (
     <>
+      <MyComponent />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
