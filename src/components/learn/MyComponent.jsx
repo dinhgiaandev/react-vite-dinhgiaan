32dfa8ce9d8a55561839b5572fb651e9ce1f2ae8
dinhgiaandev <dinhgiaanforwork@gmail.com>
2024-08-10 12:32:48 +0700

    video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,8 +1,11 @@
-
+import './style.css'
 
 const MyComponent = () => {
     return (
-        <div> Hello World. </div>
+        <>
+            <div> Hello World. </div>
+            <div className='child' style={{ borderRadius: "10px" }}> Yes Sir!</div>
+        </>
     );
 }
 export default MyComponent;
