942fa2aea84f4be037fea31542e60dcebc9a1949
dinhgiaandev <dinhgiaanforwork@gmail.com>
2024-08-10 22:20:06 +0700

    video 36

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,10 +1,23 @@
 const TodoNew = (props) => {
     const { addNewTodo } = props;
-    addNewTodo('Dinhgiaan Dev');
+    // addNewTodo('Dinhgiaan Dev');
+
+    const handleClick = () => {
+        alert('Click me')
+    }
+
+    const handleOnChange = (name) => {
+        console.log('>> Check handle ', name)
+    }
     return (
         <div className='todo-action'>
-            <input type="text" className='input-text' placeholder='Enter your task here' />
-            <button className='btn-add'>Add</button>
+            <input type="text" className='input-text' placeholder='Enter your task here'
+                onChange={(event) => { handleOnChange(event.target.value) }}
+            />
+            <button className='btn-add'
+                style={{ cursor: "pointer" }}
+                onClick={handleClick}
+            >Add</button>
         </div>
     );
 }
