61cc84697f467d9cf349a7e1d4b5250db2534978
dinhgiaandev <dinhgiaanforwork@gmail.com>
2024-08-10 12:48:14 +0700

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,9 +1,16 @@
 import './style.css'
 
 const MyComponent = () => {
+    // const dinhgiaan = "kato";
+    const arr = [1, 2, 3];
+    // const mySelf = {
+    //     name: "Dinhgiaan",
+    //     age: 20
+    // };
     return (
         <>
-            <div> Hello World. </div>
+            <div> {JSON.stringify(arr)} & Hello World. </div>
+            <div>{console.log('Dinhgiaan1')}</div>
             <div className='child' style={{ borderRadius: "10px" }}> Yes Sir!</div>
         </>
     );
