bc9c25d4fdf11659824fb3506877f8ca6c7b0b27
dinhgiaandev <dinhgiaanforwork@gmail.com>
2024-08-12 18:29:46 +0700

    video 42

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,10 @@ const App = () => {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
 
+  const deleteTodo = (id) => {
+    const newTodo = todoList.filter(item => item.id !== id)
+    setTodoList(newTodo)
+  }
   return (
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
@@ -33,26 +37,15 @@ const App = () => {
       {todoList.length > 0 ?
         <TodoData
           todoList={todoList}
+          deleteTodo={deleteTodo}
         />
-
-
         :
-        todoList.length === 0 &&
-        <div className='todo-image'>
-          <img src={reactLogo} className='logo' />
-        </div>
+        <>
+          <div className='todo-image'>
+            <img src={reactLogo} className='logo' />
+          </div>
+        </>
       }
-
-      {/* {todoList.length > 0 &&
-        <TodoData
-          todoList={todoList}
-        />
-      }
-      {todoList.length === 0 &&
-        <div className='todo-image'>
-          <img src={reactLogo} className='logo' />
-        </div>
-      } */}
     </div>
   )
 }
--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -1,13 +1,17 @@
 const TodoData = (props) => {
     //Destructuring object
-    const { todoList } = props;
+    const { todoList, deleteTodo } = props;
+    const handleClick = (id) => {
+        deleteTodo(id)
+    }
+
     return (
         <div className='todo-data'>
             {todoList.map((item, index) => {
                 return (
                     <div className={`todo-item ${index}`} key={index.id}>
                         <div>{item.name}</div>
-                        <button>Delete</button>
+                        <button onClick={() => handleClick(item.id)}>Delete</button>
                     </div>
                 )
             })}
